139f93c5047be2c46ef07763d7d44b27146e93bc
ligofff <liga@ligofff.ru>
2023-12-08 14:09:27 +0300

    Main script added

---
+++ Editor/ObjectReserializer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+
+namespace Editor
+{
+    public  class ObjectReserializer
+    {
+        [MenuItem("Ligofff/Reserialize All Assets")]
+        public static void OpenWindow()
+        {
+            if (EditorUtility.DisplayDialog("Reserialize All Assets?", "It can take a while. Are you sure?", "Yes",
+                    "No"))
+            {
+                var assets = AssetDatabase.FindAssets("").Select(AssetDatabase.GUIDToAssetPath);
+                AssetDatabase.ForceReserializeAssets(assets);
+            }
+        }
+        
+        [MenuItem("Assets/Reserialize Asset")]
+        private static void ReserializeAsset()
+        {
+            AssetDatabase.ForceReserializeAssets(new List<string>(){AssetDatabase.GetAssetPath(Selection.activeObject)});
+        }
+        
+        [MenuItem("Assets/Reserialize Asset", true)]
+        private static bool ReserializeAssetValidation()
+        {
+            return Selection.activeObject != null && AssetDatabase.Contains(Selection.activeObject);
+        }
+    }
+}
